fb6de7babc99d2c41e52d91a093f77e7e1650404
Howardkhh <howard.hou.fan@gmail.com>
2024-06-05 00:15:02 +0800

    corrected env reward

--- GetExpertData.py
+++ GetExpertData.py
@@ -21,7 +21,7 @@ total_steps = 0
 obs = env.reset()
 while not done:
     pygame.event.pump()
-    print(f"Axis 0: {joystick.get_axis(0):.2f}, Axis 1: {joystick.get_axis(1):.2f}, Axis 2: {joystick.get_axis(2):.2f}, Axis 3: {joystick.get_axis(3):.2f}, {joystick.get_button(0)}, {joystick.get_button(1)}, {joystick.get_button(7)}", end="\r")
+    # print(f"Axis 0: {joystick.get_axis(0):.2f}, Axis 1: {joystick.get_axis(1):.2f}, Axis 2: {joystick.get_axis(2):.2f}, Axis 3: {joystick.get_axis(3):.2f}, {joystick.get_button(0)}, {joystick.get_button(1)}, {joystick.get_button(7)}", end="\r")
     # action = [np.random.uniform(-80, 80), np.random.uniform(-80, 80), np.random.choice([0, 1]), np.random.choice([0, 1]), np.random.choice([0, 1])]
     action = [clip(joystick.get_axis(0)) * 100, clip(joystick.get_axis(1)) * 100, int(joystick.get_button(0)), int(joystick.get_button(1)), int(joystick.get_button(7))]
     next_obs, reward, done, info = env.step(action)
@@ -29,6 +29,7 @@ while not done:
     history["action"].append(action)
     history["reward"].append(reward)
     history["done"].append(done)
+    print(f"reward: {reward}, lap: {info[b'lap']}, checkpoint: {info[b'checkpoint']}")
     obs = next_obs
     total_reward += reward
     total_steps += 1
